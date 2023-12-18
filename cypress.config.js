b60a34bb1ecbc9f4fa520676f4299c7e149acae1
Anna Zlotnikova <atika.pazikova@gmail.com>
2023-12-18 19:58:59 +0300

    attempt to setup CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 7000,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
     },
+  },
+  video: false,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'reports/test-results-[hash].xml',
   },
 });
