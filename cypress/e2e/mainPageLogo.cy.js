bdf7898e4c82adc0bcc043f97159d8fd63ba968c
Anna ZLotnikova <68644906+AnnieZ1207@users.noreply.github.com>
2023-12-19 11:08:18 +0300

    Delete cypress/e2e/mainPageLogo.cy.js

--- cypress/e2e/mainPageLogo.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress"/>
-
-describe('first shop test', () => {
-    it('verify logo is visible', function () {
-      cy.visit('/');
-      cy.get('.logo').should('be.visible')
-    })
-  })
