cf961c7f95c547c32be17aeba9e813418fc73ba0
JackBlaaack <eugene.lapytko@gmail.com>
2024-01-09 23:45:35 +0300

    update ProductPage

--- cypress/pageObjects/ProductPage.js
+++ cypress/pageObjects/ProductPage.js
@@ -1,5 +1,12 @@
 
 class ProductPage {
-getProductPageTitle = () => cy.title()
+getProductPageTitle = () => cy.title();
+getProductPageHeader = () => cy.get('header.page-header');
+getProductPageNavigation = () => cy.get('nav.navigation');
+getProductPageBreadcrumbs = () => cy.get('div.breadcrumbs');
+getProductPageBody = () => cy.get('body[data-container="body"]');
+getProductPageFooter = () => cy.get('footer.page-footer');
+getProductPageCopyright = () => cy.get('small.copyright');
+
 }
 export default ProductPage;
