0f0e08b47eaf1ba5b62d2e02a7ce8a7599eb07c2
Pavel Korz <131908496+Paul-Korz@users.noreply.github.com>
2024-01-09 15:29:11 +0300

    TC_003.001.001 | Header > Logo > Verify Presence of Header with Logo on All Pages, TC_003.001.002 | Header > Logo > Verify Redirect to Home Page by Clicking on Logo (#10)

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -8,13 +8,20 @@ class HomePage {
   getWomanTopsLink = () => cy.get("#ui-id-9");
   getWomanJacketsLink = () => cy.get("#ui-id-11");
   getHomePageLogo = () => cy.get("a.logo");
-  getTitleName = () => cy.get('a.logo img');
+  getTitleName = () => cy.get("a.logo img");
   getImageBoxButton = () => cy.get('span[class="action more button"]');
   getImageBoxesLinks = () => cy.get('span[class="action more icon"]');
-  getErinRecommendsLink = () => cy.get('span[class="action more icon"]').contains('Shop Erin Recommends');
-  getEcoFriendlyLink = () => cy.get('span[class="action more icon"]').contains('Shop Eco-Friendly');
-  getShopPerformanceWidget = () => cy.get("a.block-promo.home-performance span[class='content bg-white']");
-  getShopPerformanceWidgetText = () => cy.get("a.block-promo.home-performance > span > span.action.more.icon");
+  getErinRecommendsLink = () =>
+    cy.get('span[class="action more icon"]').contains("Shop Erin Recommends");
+  getEcoFriendlyLink = () =>
+    cy.get('span[class="action more icon"]').contains("Shop Eco-Friendly");
+  getShopPerformanceWidget = () =>
+    cy.get("a.block-promo.home-performance span[class='content bg-white']");
+  getShopPerformanceWidgetText = () =>
+    cy.get("a.block-promo.home-performance > span > span.action.more.icon");
+  getLumaLogo = () => cy.get("a[class='logo']");
+  getHomePageUrl = () => cy.url();
+  getMainContent = () => cy.get("#maincontent");
 
   hoverWomanMainMenuLink() {
     this.getWomanMainMenuLink().trigger("mouseover");
@@ -44,10 +51,43 @@ class HomePage {
     return new EcoFriendlyPage();
   }
 
-  clickShopPerformanceWidget(){
+  clickShopPerformanceWidget() {
     this.getShopPerformanceWidget().click();
 
-    return new PerformanceFabricsPage()
+    return new PerformanceFabricsPage();
+  }
+
+  verifyLogoPresence() {
+    this.getLumaLogo().should("be.visible");
+    this.getLumaLogo().find("img").should("have.attr", "src");
+
+    return this;
+  }
+
+  visitPages(urls) {
+    urls.forEach((page) => {
+      cy.visit(page);
+      this.verifyLogoPresence();
+    });
+  }
+
+  clickLumaLogo() {
+    this.getLumaLogo().click();
+
+    return this;
+  }
+
+  verifyBaseUrl() {
+    this.getHomePageUrl().should("eq", Cypress.config().baseUrl);
+  }
+
+  returnHomePage(urls) {
+    urls.forEach((page) => {
+      cy.visit(page);
+      this.clickLumaLogo();
+      this.verifyBaseUrl();
+      this.getMainContent().should("be.visible");
+    });
   }
 }
 
