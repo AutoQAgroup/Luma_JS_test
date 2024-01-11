565fdeff0cba27890026e827aac83e4ea185f0fc
JackBlaaack <eugene.lapytko@gmail.com>
2024-01-11 16:21:04 +0300

    update verifyProductPageStructure

--- cypress/e2e/verifyProductPageStructure.cy.js
+++ cypress/e2e/verifyProductPageStructure.cy.js
@@ -10,24 +10,34 @@ describe("verifyProductPageStructure", () => {
     cy.visit(ProductPageData.ExampleUrl);
   });
 
-  it("TC_002.002.001 | Product page > Self > Verify Structure", () => {
+  it("TC_002.002.001 | Product page > Self > Verify header of the page", () => {
     productPage
-    .getProductPageHeader()
-    .should('have.prop', 'tagName',ProductPageData.structurePage[0]);
+      .getProductPageHeader()
+      .should("have.prop", "tagName", ProductPageData.structurePage[0]);
+  });
+  it("TC_002.002.002 | Product page > Self > Verify navigation section of the page", () => {
     productPage
-    .getProductPageNavigation()
-    .should('have.class', ProductPageData.structurePage[1]);
+      .getProductPageNavigation()
+      .should("have.class", ProductPageData.structurePage[1]);
+  });
+  it("TC_002.002.003 | Product page > Self > Verify breadcrumbs of the page", () => {
     productPage
-    .getProductPageBreadcrumbs()
-    .should('have.class', ProductPageData.structurePage[2]);
+      .getProductPageBreadcrumbs()
+      .should("have.class", ProductPageData.structurePage[2]);
+  });
+  it("TC_002.002.004 | Product page > Self > Verify body of the page", () => {
     productPage
-    .getProductPageBody()
-    .should('have.prop', 'tagName', ProductPageData.structurePage[3]);
+      .getProductPageBody()
+      .should("have.prop", "tagName", ProductPageData.structurePage[3]);
+  });
+  it("TC_002.002.005 | Product page > Self > Verify footer of the page", () => {
     productPage
-    .getProductPageFooter()
-    .should('have.prop', 'tagName', ProductPageData.structurePage[4]);
+      .getProductPageFooter()
+      .should("have.prop", "tagName", ProductPageData.structurePage[4]);
+  });
+  it("TC_002.002.006 | Product page > Self > Verify copyright of the page", () => {
     productPage
-    .getProductPageCopyright()
-    .should('have.class', ProductPageData.structurePage[5]);
+      .getProductPageCopyright()
+      .should("have.class", ProductPageData.structurePage[5]);
   });
 });
