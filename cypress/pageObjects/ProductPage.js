d8d94e2c6891f02eab64b2122ecfa5fc8895063b
JackBlaaack <eugene.lapytko@gmail.com>
2024-01-12 09:46:58 +0300

    update productPage

--- cypress/pageObjects/ProductPage.js
+++ cypress/pageObjects/ProductPage.js
@@ -2,21 +2,15 @@ import HomePage from "./HomePage";
 import ProductListPage from "./ProductListPage";
 
 class ProductPage {
-<<<<<<< HEAD
-getProductPageTitle = () => cy.title();
-getProductPageHeader = () => cy.get('header.page-header');
-getProductPageNavigation = () => cy.get('nav.navigation');
-getProductPageBreadcrumbs = () => cy.get('div.breadcrumbs');
-getProductPageBody = () => cy.get('body[data-container="body"]');
-getProductPageFooter = () => cy.get('footer.page-footer');
-getProductPageCopyright = () => cy.get('small.copyright');
-
-=======
-    getProductPrice = () => cy.get('.product-info-price .price');
-    getProductPriceLabel = () => cy.get('.product-info-price .price-label');
-    getProductPageTitle = () => cy.title();
-    
->>>>>>> origin/main
+  getProductPageTitle = () => cy.title();
+  getProductPageHeader = () => cy.get("header.page-header");
+  getProductPageNavigation = () => cy.get("nav.navigation");
+  getProductPageBreadcrumbs = () => cy.get("div.breadcrumbs");
+  getProductPageBody = () => cy.get('body[data-container="body"]');
+  getProductPageFooter = () => cy.get("footer.page-footer");
+  getProductPageCopyright = () => cy.get("small.copyright");
+  getProductPrice = () => cy.get(".product-info-price .price");
+  getProductPriceLabel = () => cy.get(".product-info-price .price-label");
 }
 
 export default ProductPage;
