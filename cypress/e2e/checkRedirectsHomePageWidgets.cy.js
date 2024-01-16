893bcb240f08e62a9ca052d011ab176db0cf4edb
JackBlaaack <eugene.lapytko@gmail.com>
2024-01-16 11:16:42 +0300

    change cycle

--- cypress/e2e/checkRedirectsHomePageWidgets.cy.js
+++ cypress/e2e/checkRedirectsHomePageWidgets.cy.js
@@ -4,7 +4,7 @@ import HomePage from "../pageObjects/HomePage";
 import {widgetBlock, actionIcons} from "../fixtures/homePageData.json"
 describe('checkRedirectsHomePageWidgets', () =>{
    const homePage = new HomePage(); 
-   actionIcons.actionIcons.forEach((widget, index) => {
+   actionIcons.forEach((widget, index) => {
    it(`RF_001.002 |001.003 | 001.004.001 | Main page > Self >Check ${widgetName}, when widgets are clicked`, () =>{
     homePage
     .clickWidgetLinks(widget, index)
