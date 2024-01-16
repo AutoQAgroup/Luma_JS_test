c6a357e32c673b701b3bfe3f85d1100e6bef4866
Paul-Korz <paulkorzenok@gmail.com>
2024-01-16 18:35:55 +0300

    whatIsNewPageAgain

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,8 +11,13 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
+import HomePage from "../pageObjects/HomePage";
+const homePage = new HomePage();
+
+Cypress.Commands.add("redirectToWhatIsNewPage", () => {
+  homePage.clickWhatsNewPageLink();
+});
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
