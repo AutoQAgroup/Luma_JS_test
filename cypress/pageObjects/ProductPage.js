e4c371dfd1c018ecdf5be15e58b16b2eb15ed614
ElenaDetusheva <e.det@list.ru>
2024-02-13 19:29:25 +0300

    add_tc_02_08_02

--- cypress/pageObjects/ProductPage.js
+++ cypress/pageObjects/ProductPage.js
@@ -18,7 +18,8 @@ class ProductPage {
     getProductPageFooter = () => cy.get("footer.page-footer");
     getProductPageCopyright = () => cy.get("small.copyright");
     getProductName = () => cy.get('span.base');
-
+    getSKU = () => cy.get('div.product.attribute.sku');
+    getSKUvalue = () =>cy.get('div[itemprop="sku"]');
   compareNameAndEndPoint() {
     this.getProductName().then(($el)=>{
       let text = $el.text().toLowerCase().split(' ').join('-')
